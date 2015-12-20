12d78d1220caad27ca48eaddf6d98a95441fd06c
AdiMac <adamsken1221@gmail.com>
2015-12-20 17:26:30 +0100

    try dbconn

--- conftest.py
+++ conftest.py
@@ -1,3 +1,6 @@
+from time import strftime
+
+import mysql
 import pytest
 
 
@@ -29,3 +32,46 @@ def pytest_runtest_setup(item):
         item.config.option.base_url = item.config.option.base_url.replace('/b', '')
     if hasattr(item.obj, 'skipsprod') and 'allizom.org' not in item.config.option.base_url:
         pytest.skip("skipping tests marked staging only")
+
+@pytest.fixture(autouse=True)
+def session_id(mozwebqa):
+    print 'Session ID: {}'.format(mozwebqa.selenium.session_id)
+    str = '{}\n'.format(mozwebqa.selenium.session_id)
+    str_session_id = '{}'.format(mozwebqa.selenium.session_id)
+
+    with open ("/home/adi/python.txt", "a") as myfile:
+        myfile.write(str)
+
+    current_time = strftime("%Y-%m-%d %H:%M")
+    print('Current time is: {}'.format(current_time))
+
+    """ Connect to MySQL database """
+    try:
+        conn = mysql.connector.connect(host='localhost',
+                                       database='python_mysql',
+                                       user='root',
+                                       password='')
+        if conn.is_connected():
+            print('Connected to MySQL database')
+
+        c = conn.cursor()
+        tblQuery = """CREATE TABLE IF NOT EXISTS test_session_ids (id int unsigned auto_increment not NULL,
+        session_id VARCHAR(60) not NULL,
+        date_created VARCHAR(100) not NULL,
+        primary key(id))"""
+        c.execute(tblQuery)
+        print('............Successfully created table .......')
+        insQuery = """insert into test_session_ids (session_id, date_created) values ('%s', '%s')"""
+        # insQuery = """insert into test_session_ids (session_id, date_created) values ('whatever', 'whatever')"""
+        c.execute("insert into test_session_ids (session_id, date_created) values (%s, %s)", (str_session_id, current_time))
+        # c.execute(insQuery)
+        print('............Successfully ADDED to table .......')
+        conn.commit()
+
+    except Error as e:
+        print(e)
+
+    finally:
+        conn.close()
+
+
